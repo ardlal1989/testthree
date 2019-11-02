4da8c69e6fa472a6348d20e6b2047519280d9511
ardlal1989 <50587555+ardlal1989@users.noreply.github.com>
2019-11-02 23:25:43 +0530

    Update Demo6Application.java

--- src/main/java/com/example/demo/Demo6Application.java
+++ src/main/java/com/example/demo/Demo6Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -19,4 +21,18 @@ public class Demo6Application {
 	{
 		return "home";
 	}  
+
+@RequestMapping("getObject")
+public ModelAndView getObject(@RequestParam("endpoint") String endpoint,@RequestParam("bucketName") String bucketName,@RequestParam("imageName") String imageName)
+{
+	ModelAndView mv=new ModelAndView();
+			
+	String finalpath=endpoint+"/"+bucketName+"/"+imageName;
+	
+	System.out.println(finalpath);
+	mv.addObject("path",finalpath);
+	mv.setViewName("getObject");
+	return mv;
+}
+
 }
